e546d70b3d4f6a81d6b7a2cfbb11984dc809db66
VATl1 <166824532+VATl1@users.noreply.github.com>
2025-01-22 17:41:51 +0200

    Add files via upload

---
+++ l1_variables_and_math/task4.py
@@ -0,0 +1,15 @@
+if __name__ == "__main__":
+    # Do not change the line below
+    a = b = c = d = None
+
+    # Assign the values of correct types to variables a, b, c, d 
+    # to make the script to work without errors
+    a = 10
+    b = 1.5
+    c = 1j
+    d = "AAA"
+    # Do not change the lines below
+    assert isinstance(a, int)
+    assert isinstance(b, float)
+    assert isinstance(c, complex)
+    assert isinstance(d, str)

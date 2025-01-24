7e652b0fb5c5d6d2f8050e037ee3c99163d0c13b
Anton <andf322@gmail.com>
2025-01-24 17:13:32 +0200

    add

--- l4_loops/task2.py
+++ l4_loops/task2.py
@@ -1,6 +1,10 @@
 def odd_str(n: int) -> str:
     s = ""
-
+    s = ""
+    for i in range(n + 1):  
+        if i % 2 != 0:  
+            s += str(i)  
+    return s
     # Use for loop to run from 0 to n (included) 
     # and form a string `s`.
     # String `s` will contain only odd numbers from interval [0; n].
@@ -9,7 +13,7 @@ def odd_str(n: int) -> str:
     # (0, 1, 2, 3, 4) -> "13"
     # (0, 1, 2, 3, 4, 5, 6) -> "135" 
 
-    return s
+    
 
 
 # Do not change the below's code

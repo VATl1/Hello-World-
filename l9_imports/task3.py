c96a640d46cb8f421a92270ad7f5c623ad4fed88
Anton <andf322@gmail.com>
2025-01-31 17:33:13 +0200

    fix

--- l9_imports/task3.py
+++ l9_imports/task3.py
@@ -1,8 +1,3 @@
-import sys
-from pathlib import Path
-
-sys.path.append(str(Path(__file__).parent.parent))
-
 # Import the function `multiply` from `l2_functions` module
 # to make the script work without errors
 from l2_functions.task6 import multiply
